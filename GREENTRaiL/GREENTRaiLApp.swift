c69d87668a936039e5a33bb7fbc17e426caee73b
tiingweii-shii <57331456+tiingweii-shii@users.noreply.github.com>
2023-10-21 10:29:08 -0400

    Initial Commit

---
+++ GREENTRaiL/GREENTRaiLApp.swift
@@ -0,0 +1,17 @@
+//
+//  GREENTRaiLApp.swift
+//  GREENTRaiL
+//
+//  Created by Tingwei Shi on 10/21/23.
+//
+
+import SwiftUI
+
+@main
+struct GREENTRaiLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
