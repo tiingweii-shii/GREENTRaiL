14d9468e0ad2f9b6e0716ed485ccbfcfa128f95b
lukeatki <luke24aal@gmail.com>
2023-10-21 12:12:52 -0400

    home page and mdeia

--- GREENTRaiL/ContentView.swift
+++ GREENTRaiL/ContentView.swift
@@ -9,19 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack { }
+        ZStack {
+            Image("Vector 3")
+
+            Image("home logo")
+
+        }
+        .padding()
         .frame(width: 390, height: 844)
         .background(
-          LinearGradient(
-            stops: [
-              Gradient.Stop(color: Color(red: 0.59, green: 0.88, blue: 1), location: 0.00),
-              Gradient.Stop(color: Color(red: 0.59, green: 0.88, blue: 1).opacity(0), location: 1.00),
-            ],
-            startPoint: UnitPoint(x: 0.5, y: 0.27),
-            endPoint: UnitPoint(x: 0.5, y: 0.45)
-          )
+            LinearGradient(
+                stops: [
+                    Gradient.Stop(color: Color(red: 0.59, green: 0.88, blue: 1), location: 0.00),
+                    Gradient.Stop(color: Color(red: 0.59, green: 0.88, blue: 1).opacity(0), location: 1.00),
+                ],
+                startPoint: UnitPoint(x: 0.5, y: 0.27),
+                endPoint: UnitPoint(x: 0.5, y: 0.45)
+            )
         )
-        .padding()
+        
     }
 }
 
