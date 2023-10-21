813f7934c0a3b23fd87bb3e241b3e316448abc7e
lukeatki <luke24aal@gmail.com>
2023-10-21 11:43:09 -0400

    gradietn added t home

--- GREENTRaiL/ContentView.swift
+++ GREENTRaiL/ContentView.swift
@@ -9,12 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+        ZStack { }
+        .frame(width: 390, height: 844)
+        .background(
+          LinearGradient(
+            stops: [
+              Gradient.Stop(color: Color(red: 0.59, green: 0.88, blue: 1), location: 0.00),
+              Gradient.Stop(color: Color(red: 0.59, green: 0.88, blue: 1).opacity(0), location: 1.00),
+            ],
+            startPoint: UnitPoint(x: 0.5, y: 0.27),
+            endPoint: UnitPoint(x: 0.5, y: 0.45)
+          )
+        )
         .padding()
     }
 }
