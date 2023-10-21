76fb853ba9de50e441d267071da4ef698a0b68e3
lukeatki <luke24aal@gmail.com>
2023-10-21 19:03:14 -0400

    tiny map

---
+++ GREENTRaiL/TinyMapSlideUpView.swift
@@ -0,0 +1,24 @@
+//
+//  TinyMapSlideUpView.swift
+//  GREENTRaiL
+//
+//  Created by Luke Atkins on 10/21/23.
+//
+
+import SwiftUI
+import MapKit
+
+
+struct TinyMapSlideUpView: View {
+    
+    var body: some View {
+        Map()
+    }
+    
+    
+    
+}
+
+#Preview {
+    TinyMapSlideUpView()
+}
