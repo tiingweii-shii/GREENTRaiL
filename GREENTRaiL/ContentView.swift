668c9cbb2ebf419ab4db0998e4c361b028278fed
tiingweii-shii <57331456+tiingweii-shii@users.noreply.github.com>
2023-10-21 12:15:46 -0400

    loginview

--- GREENTRaiL/ContentView.swift
+++ GREENTRaiL/ContentView.swift
@@ -9,25 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Image("Vector 3")
-
-            Image("home logo")
-
-        }
-        .padding()
-        .frame(width: 390, height: 844)
-        .background(
-            LinearGradient(
-                stops: [
-                    Gradient.Stop(color: Color(red: 0.59, green: 0.88, blue: 1), location: 0.00),
-                    Gradient.Stop(color: Color(red: 0.59, green: 0.88, blue: 1).opacity(0), location: 1.00),
-                ],
-                startPoint: UnitPoint(x: 0.5, y: 0.27),
-                endPoint: UnitPoint(x: 0.5, y: 0.45)
-            )
-        )
-        
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
---
+++ GREENTRaiL/LogInView.swift
@@ -0,0 +1,35 @@
+//
+//  LogInView.swift
+//  GREENTRaiL
+//
+//  Created by Tingwei Shi on 10/21/23.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        ZStack {
+            Image("Vector 3")
+            Image("home logo")
+
+        }
+        .padding()
+        .frame(width: 390, height: 844)
+        .background(
+            LinearGradient(
+                stops: [
+                    Gradient.Stop(color: Color(red: 0.59, green: 0.88, blue: 1), location: 0.00),
+                    Gradient.Stop(color: Color(red: 0.59, green: 0.88, blue: 1).opacity(0), location: 1.00),
+                ],
+                startPoint: UnitPoint(x: 0.5, y: 0.27),
+                endPoint: UnitPoint(x: 0.5, y: 0.45)
+            )
+        )
+        
+    }
+}
+
+#Preview {
+    LogInView()
+}
