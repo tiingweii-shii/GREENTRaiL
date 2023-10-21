daf81c1a03b39a6d1a3fef1d712830050a955649
lukeatki <luke24aal@gmail.com>
2023-10-21 12:45:21 -0400

    added map view file

---
+++ GREENTRaiL/ActivitiesMapView.swift
@@ -0,0 +1,19 @@
+//
+//  ActivitiesMapView.swift
+//  GREENTRaiL
+//
+//  Created by Luke Atkins on 10/21/23.
+//
+
+import SwiftUI
+import MapKit
+
+struct ActivitiesMapView: View {
+    var body: some View {
+        Map()
+    }
+}
+
+#Preview {
+    ActivitiesMapView()
+}
