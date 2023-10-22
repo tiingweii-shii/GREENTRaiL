39db4e730a81e3402715d838bc52aee7767c7f06
eraserbird <ruthluvu@mit.edu>
2023-10-21 20:25:08 -0400

    query users / hikes / trails

--- GREENTRaiL/GREENTRaiLApp.swift
+++ GREENTRaiL/GREENTRaiLApp.swift
@@ -9,6 +9,36 @@ import SwiftUI
 import FirebaseCore
 import FirebaseFirestore
 
+struct user: Codable {
+    var age: Int
+    var gender: String
+    var height: Float
+    var weight: Float
+    var firstName: String
+    var lastName: String
+    var aerobicfitnessrating: Int
+    var hikes: [String]
+    var location: [Float]
+}
+
+struct trails: Codable {
+    var active: Bool = false
+    var difficulty: Int = 3
+    var location: [Float] = [3.0]
+    var hikes: [String] = ["hi"]
+    var name: String
+}
+
+struct hikes: Codable {
+    var bpm: [Int]
+    var distance: [Float]
+    var duration: Int
+    var elevation: [Float]
+    var name: String
+    var time: [Float]
+    var trail: String
+}
+
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
@@ -30,16 +60,123 @@ struct GREENTRaiLApp: App {
     }
 }
 
-func get_trails() {
-    let db = Firestore.firestore()
-    db.collection("trails").whereField("active", isEqualTo: true)
-        .getDocuments() { (querySnapshot, err) in
-            if let err = err {
-                print("Error getting documents: \(err)")
+/*func get_trails() -> trails {
+    let docRef = Firestore.firestore().document("trails/Apple Loop")
+
+    var myData = trails()
+    docRef.getDocument { (document, error) in
+        if let error = error {
+            print("Error fetching document: \(error)")
+        } else if let document = document, document.exists {
+            if let data = document.data() {
+                do {
+                    let jsonData = try JSONSerialization.data(withJSONObject: data, options: [])
+                    myData = try JSONDecoder().decode(trails.self, from: jsonData)
+                    print(myData)
+                    // Access other fields here
+                } catch {
+                    print("Error decoding data: \(error)")
+                }
             } else {
-                for document in querySnapshot!.documents {
-                    print("\(document.documentID) => \(document.data())")
+                print("Data is not a valid JSON object")
+            }
+        } else {
+            print("Document does not exist")
+        }
+    }
+    return myData
+}*/
+
+
+func get_trails() -> [trails] {
+    let collectionRef = Firestore.firestore().collection("trails")
+
+    var myDataArray: [trails] = []
+    
+    collectionRef.getDocuments { (querySnapshot, error) in
+        if let error = error {
+            print("Error fetching collection: \(error)")
+        } else {
+            for document in querySnapshot!.documents {
+                let data = document.data()
+                
+                do {
+                    let jsonData = try JSONSerialization.data(withJSONObject: data, options: [])
+                    let myData = try JSONDecoder().decode(trails.self, from: jsonData)
+                    myDataArray.append(myData)
+                } catch {
+                    print("Error decoding data: \(error)")
                 }
             }
+
+            // Now you have an array of MyData objects containing data from the collection
+            for data in myDataArray {
+                print(data)
+                // Access other fields here
+            }
         }
+    }
+    return myDataArray
+}
+
+func get_users() -> [user] {
+    let collectionRef = Firestore.firestore().collection("users")
+
+    var myDataArray: [user] = []
+    
+    collectionRef.getDocuments { (querySnapshot, error) in
+        if let error = error {
+            print("Error fetching collection: \(error)")
+        } else {
+            for document in querySnapshot!.documents {
+                let data = document.data()
+                
+                do {
+                    let jsonData = try JSONSerialization.data(withJSONObject: data, options: [])
+                    let myData = try JSONDecoder().decode(user.self, from: jsonData)
+                    myDataArray.append(myData)
+                } catch {
+                    print("Error decoding data: \(error)")
+                }
+            }
+
+            // Now you have an array of MyData objects containing data from the collection
+            for data in myDataArray {
+                print(data)
+                // Access other fields here
+            }
+        }
+    }
+    return myDataArray
+}
+
+func get_hikes() -> [hikes] {
+    let collectionRef = Firestore.firestore().collection("hikes")
+
+    var myDataArray: [hikes] = []
+    
+    collectionRef.getDocuments { (querySnapshot, error) in
+        if let error = error {
+            print("Error fetching collection: \(error)")
+        } else {
+            for document in querySnapshot!.documents {
+                let data = document.data()
+                
+                do {
+                    let jsonData = try JSONSerialization.data(withJSONObject: data, options: [])
+                    let myData = try JSONDecoder().decode(hikes.self, from: jsonData)
+                    myDataArray.append(myData)
+                } catch {
+                    print("Error decoding data: \(error)")
+                }
+            }
+
+            // Now you have an array of MyData objects containing data from the collection
+            for data in myDataArray {
+                print(data)
+                // Access other fields here
+            }
+        }
+    }
+    return myDataArray
 }
