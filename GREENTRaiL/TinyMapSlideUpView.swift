671540dff01a11fcb3ee3524d86c73a12cbddab1
lukeatki <luke24aal@gmail.com>
2023-10-22 02:23:43 -0400

    pins nice
    pins in png project file

--- GREENTRaiL/TinyMapSlideUpView.swift
+++ GREENTRaiL/TinyMapSlideUpView.swift
@@ -11,22 +11,43 @@ import MapKit
 
 
 extension CLLocationCoordinate2D{
-    static let trail = CLLocationCoordinate2D(latitude: 10.00000, longitude: 245.00000)
-    static let tom = CLLocationCoordinate2D(latitude: 82.00000, longitude: 31.00000)
-    static let bob = CLLocationCoordinate2D(latitude: 92.00000, longitude: -1.00000)
-    static let rain = CLLocationCoordinate2D(latitude: -42.00000, longitude: 71.00000)
-    static let cat = CLLocationCoordinate2D(latitude: 48.00000, longitude: -100.00000)
+    
+    static let trail = CLLocationCoordinate2D(latitude: 38.04771931544412, longitude: -122.66606389871552)
+    static let tom = CLLocationCoordinate2D(latitude: 37.9681886024788, longitude: -122.78073370253512)
+    static let bob = CLLocationCoordinate2D(latitude: 37.886403993495904, longitude: -122.57268011267833)
+    static let rain = CLLocationCoordinate2D(latitude: 37.954654387796396, longitude: -122.14146673004936)
+    static let cat = CLLocationCoordinate2D(latitude: 38.12926506556985, longitude: -121.97737553970704)
 }
 
-//func get_trail_pointers() -> [trails]{
-//    var all_trails: [trails] = []
-//    getTrails() { trails in
-//        for trail in trails {
-//            all_trails.append(trail)
-//        }
-//    }
-//    return all_trails
-//}
+// Create the annotation class
+class CustomAnnotation: NSObject, MKAnnotation {
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    var image: UIImage?
+    
+    init(coordinate: CLLocationCoordinate2D, title: String?, subtitle: String?, image: UIImage?) {
+        self.coordinate = coordinate
+        self.title = title
+        self.subtitle = subtitle
+        self.image = image
+    }
+}
+
+// Create the custom view
+struct CustomAnnotationView: View {
+    var annotation: CustomAnnotation
+    
+    var body: some View {
+        ZStack {
+            Image(uiImage: annotation.image!)
+            Text(annotation.title!)
+                .font(.headline)
+            Text(annotation.subtitle!)
+                .font(.subheadline)
+        }
+    }
+}
 
 struct TinyMapSlideUpView: View {
     //    let all_trail: [trails]
@@ -35,11 +56,32 @@ struct TinyMapSlideUpView: View {
     //    }
     var body: some View {
         Map{
-            Marker("hard trail", coordinate: .bob)
-            Marker("easy trail", coordinate: .trail)
-            Marker("medium trail", coordinate: .rain)
-            Marker("do not attempt trail", coordinate: .cat)
-            Marker("wild trail", coordinate: .tom)
+            
+            Annotation("do not attempt", coordinate: .rain){
+                ZStack{
+                    Image("pins")
+                }
+            }
+            Annotation("easy walk", coordinate: .tom){
+                ZStack{
+                    Image("pins")
+                }
+            }
+            Annotation("tough trail", coordinate: .cat){
+                ZStack{
+                    Image("pins")
+                }
+            }
+            Annotation("just right", coordinate: .bob){
+                ZStack{
+                    Image("pins")
+                }
+            }
+            Annotation("too eas trail", coordinate: .trail){
+                ZStack{
+                    Image("pins")
+                }
+            }
         }
         
         // take in the locatoins of all of the hiking trails
