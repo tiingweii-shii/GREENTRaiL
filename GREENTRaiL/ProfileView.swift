906fcdb80ee51595db44dcd0dbe06be4d9951047
tiingweii-shii <57331456+tiingweii-shii@users.noreply.github.com>
2023-10-22 02:16:17 -0400

    Update ProfileView.swift

--- GREENTRaiL/ProfileView.swift
+++ GREENTRaiL/ProfileView.swift
@@ -19,137 +19,133 @@ var profile = profileInfo(userId: "12345", name: "Greeny", age: "10", height: "5
 
 struct ProfileView: View {
     var body: some View {
-        ZStack {
-            Rectangle()
-            .foregroundColor(.clear)
-            .frame(width: 400, height: 830)
-            .background(Color(red: 0.13, green: 0.32, blue: 0.41))
-            .padding([.top], 20)
-            .cornerRadius(56)
-            ScrollView {
-                LazyVStack(alignment: .leading, spacing: 16) {
+        NavigationStack {
+            ZStack {
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 400, height: 830)
+                    .background(Color(red: 0.13, green: 0.32, blue: 0.41))
+                    .padding([.top], 20)
+                    .cornerRadius(56)
+                ScrollView {
+                    LazyVStack(alignment: .leading, spacing: 16) {
+                        Divider()
+                            .frame(height: 10)
+                            .overlay(.white)
+                            .padding([.top, .bottom], 10)
+                        Text("Profile")
+                            .font(
+                                Font.custom("Fredoka", size: 32)
+                                    .weight(.bold)
+                            )
+                            .frame(width: 105, height: 33, alignment: .topLeading)
+                        VStack(spacing: 5) {
+                            HStack {
+                                Text("UserID:")
+                                Text(profile.userId)
+                            }
+                            HStack {
+                                Text("Name:")
+                                Text(profile.name)
+                            }
+                            HStack {
+                                Text("Age:")
+                                Text(profile.age)
+                            }
+                            HStack {
+                                Text("Height:")
+                                Text(profile.height)
+                            }
+                            HStack {
+                                Text("Weight:")
+                                Text(profile.height)
+                            }
+                        }
+                        .font(
+                            Font.custom("Fredoka", size: 24)
+                                .weight(.medium)
+                        )
+                    }
                     Divider()
                         .frame(height: 10)
                         .overlay(.white)
                         .padding([.top, .bottom], 10)
-                    Text("Profile")
-                        .font(
-                            Font.custom("Fredoka", size: 32)
-                                .weight(.bold)
-                        )
-                        .frame(width: 105, height: 33, alignment: .topLeading)
-                    VStack(spacing: 5) {
-                        HStack {
-                            Text("UserID:")
-                            Text(profile.userId)
-                        }
-                        HStack {
-                            Text("Name:")
-                            Text(profile.name)
-                        }
-                        HStack {
-                            Text("Age:")
-                            Text(profile.age)
-                        }
-                        HStack {
-                            Text("Height:")
-                            Text(profile.height)
-                        }
-                        HStack {
-                            Text("Weight:")
-                            Text(profile.height)
-                        }
-                    }
-                    .font(
-                        Font.custom("Fredoka", size: 24)
-                        .weight(.medium)
-                        )
-                }
-                Divider()
-                    .frame(height: 10)
-                    .overlay(.white)
-                    .padding([.top, .bottom], 10)
-
-                LazyVStack(alignment: .leading, spacing: 18) {
-                    Text("Statistics")
-                        .font(
-                            Font.custom("Fredoka", size: 32)
-                                .weight(.bold)
-                        )
-                        .frame(width: 150, height: 33, alignment: .topLeading)
-                    VStack(spacing: 5) {
-                        VStack {
-                            Text("Average heart rate:")
-                            VStack {
-                                HStack {
-                                    Text("this week:")
-                                    Text("70 per minute")
-                                }
-                                HStack {
-                                    Text("this month:")
-                                    Text("79 per minute")
-                                }
-                            }.font(
-                                Font.custom("Fredoka", size: 24)
-                                .weight(.light)
-                                )
-                        }
-                    }
-                    .font(
-                        Font.custom("Fredoka", size: 24)
-                        .weight(.medium)
-                        )
                     
-                     Divider()
-                         .frame(height: 10)
-                         .overlay(.white)
-                         .padding([.top, .bottom], 10)
-                     }
-                
-                Button(action: {
-                    /*getTrails() { hikes in
-                        for hike in hikes {
-                            print(hike)
+                    LazyVStack(alignment: .leading, spacing: 18) {
+                        Text("Statistics")
+                            .font(
+                                Font.custom("Fredoka", size: 32)
+                                    .weight(.bold)
+                            )
+                            .frame(width: 150, height: 33, alignment: .topLeading)
+                        VStack(spacing: 5) {
+                            VStack {
+                                Text("Average heart rate:")
+                                VStack {
+                                    HStack {
+                                        Text("this week:")
+                                        Text("70 per minute")
+                                    }
+                                    HStack {
+                                        Text("this month:")
+                                        Text("79 per minute")
+                                    }
+                                }.font(
+                                    Font.custom("Fredoka", size: 24)
+                                        .weight(.light)
+                                )
+                            }
                         }
-                    }*/
-                }, label: {
-                    Text("Settings")
-                        .foregroundColor(Color.white)
-                        .font(Font.custom("Fredoka-Bold", size: 32))
-                        .frame(width: 122, height: 40, alignment: .topLeading)
-                        .background(
-                            RoundedRectangle(cornerRadius: 50)
-                                          .stroke(.white, lineWidth: 5)
-                                .frame(width: 350, height: 60)
+                        .font(
+                            Font.custom("Fredoka", size: 24)
+                                .weight(.medium)
                         )
-                }).padding(20)
-                
-                Button(action: {
-                    /*getTrails() { hikes in
-                        for hike in hikes {
-                            print(hike)
+                        
+                        Divider()
+                            .frame(height: 10)
+                            .overlay(.white)
+                            .padding([.top, .bottom], 10)
+                    }
+                    
+                    Button(action: {
+                    }, label: {
+                        Text("Settings")
+                            .foregroundColor(Color.white)
+                            .font(Font.custom("Fredoka-Bold", size: 32))
+                            .frame(width: 122, height: 40, alignment: .topLeading)
+                            .background(
+                                RoundedRectangle(cornerRadius: 50)
+                                    .stroke(.white, lineWidth: 5)
+                                    .frame(width: 350, height: 60)
+                            )
+                    }).padding(20)
+                    
+                    NavigationLink(destination: LogInView()) {
+                        Text("Log Out")
+                            .foregroundColor(Color.white)
+                            .font(Font.custom("Fredoka-Bold", size: 32))
+                            .frame(width: 122, height: 40, alignment: .topLeading)
+                            .background(
+                                RoundedRectangle(cornerRadius: 50)
+                                    .stroke(.white, lineWidth: 5)
+                                    .frame(width: 350, height: 60)
+                            )
+                        .navigationDestination(for: String.self) { view in
+                            if view == "NewView" {
+                                Text("This is NewView")
+                            }
                         }
-                    }*/
-                }, label: {
-                    Text("Log Out")
-                        .foregroundColor(Color.white)
-                        .font(Font.custom("Fredoka-Bold", size: 32))
-                        .frame(width: 122, height: 40, alignment: .topLeading)
-                        .background(
-                            RoundedRectangle(cornerRadius: 50)
-                                          .stroke(.white, lineWidth: 5)
-                                .frame(width: 350, height: 60)
-                        )
-                }).padding(20)
-
+                    }.padding(20)
+                    
+                }
+                .padding(.top, 80)
+                .padding(.leading, 15)
+                .frame(width: 400, height: 850)
+                .foregroundColor(.white)
             }
-            .padding(.top, 80)
-            .padding(.leading, 15)
             .frame(width: 400, height: 850)
-            .foregroundColor(.white)
+            .background(Color(red: 0.71, green: 0.91, blue: 1))
         }
-        .frame(width: 400, height: 850)
-        .background(Color(red: 0.71, green: 0.91, blue: 1))
     }
 }
 
